b8208b26d20598160bb57262dcb5668f8b3971b8
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2017-05-25 16:20:30 +0700

    - Add review page

---
+++ test/example/tab-data-review.php
@@ -0,0 +1,198 @@
+<?php 
+//Validation url param
+$search = filter_var((empty($_GET['search'])?'':$_GET['search']),FILTER_SANITIZE_STRING);
+$page = filter_var((empty($_GET['page'])?'1':$_GET['page']),FILTER_SANITIZE_STRING);
+$itemsperpage = filter_var((empty($_GET['itemsperpage'])?'10':$_GET['itemsperpage']),FILTER_SANITIZE_STRING);
+?>
+<div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <form method="get" action="<?php $_SERVER['PHP_SELF'].'?search='.$search?>">
+                        <div class="col-lg-10 col-md-9 col-sm-9 col-xs-12">
+                            <div class="form-group">
+                                <input name="search" type="text" placeholder="Search here..." class="form-control border-input" value="<?php echo $search?>">
+                            </div>
+                            <div class="form-group hidden">
+                                <input name="m" type="text" class="form-control border-input" value="24" hidden>
+                                <input name="page" type="text" class="form-control border-input" value="1" hidden>
+                                <input name="itemsperpage" type="text" class="form-control border-input" value="10" hidden>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-2">
+                            <div class="form-group">
+                                <button name="submitsearch" type="submit" class="btn btn-fill btn-wd ">Search</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div><hr>
+            <div class="container-fluid">
+                <div class="row">
+               
+<?php 
+    $url = Core::getInstance()->api.'/book/review/data/search/'.$page.'/'.$itemsperpage.'/'.$datalogin['token'].'/?query='.$search;
+    $data = json_decode(Core::execGetRequest($url));
+
+     // Data Status
+    $urlstatus = Core::getInstance()->api.'/book/review/status/'.$datalogin['token'];
+    $datastatus = json_decode(Core::execGetRequest($urlstatus));
+
+    
+    if (!empty($data))
+        {
+            if ($data->{'status'} == "success")
+            {
+
+                foreach ($data->results as $row => $value) {
+                    if (isset($_POST['submitupdatereview'.$value->{'ReviewID'}]))
+                    {
+                        $post_array = array(
+                            'Username' => $datalogin['username'],
+                            'Token' => $datalogin['token'],
+                            'Detail' => $_POST['detail'],
+                            'Status' => $_POST['status'],
+                            'ReviewID' => $value->{'ReviewID'}
+                        );
+                        Core::updateProcess(Core::getInstance()->api.'/book/review/update',$post_array,'from Data Review');
+                        echo Core::reloadPage();
+                    }
+                }
+
+                foreach ($data->results as $row => $value) {
+                    if (isset($_POST['submitdeletereview'.$value->{'ReviewID'}]))
+                    {
+                        $post_array = array(
+                            'Username' => $datalogin['username'],
+                            'Token' => $datalogin['token'],
+                            'ReviewID' => $value->{'ReviewID'}
+                        );
+                        Core::deleteProcess(Core::getInstance()->api.'/book/review/delete',$post_array,'Data Review');
+                        echo Core::reloadPage();
+                    }
+                }
+
+                echo '<div class="col-md-12">
+                        <div class="card card-plain">
+                            <div class="header">
+                                <h4 class="title text-uppercase">Data Review</h4>
+                                <p class="category">Message: '.$data->{'message'}.'<br>
+                                Shows no: '.$data->metadata->{'number_item_first'}.' - '.$data->metadata->{'number_item_last'}.' from total data: '.$data->metadata->{'records_total'}.'</p>
+                                    <div class="dropdown">
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+		    				    			<p><i class="ti-zip"></i> Export Data <b class="caret"></b></p>
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li><a href="#" onClick ="$(\'#export\').tableExport({type:\'excel\',escape:\'false\'});">Export XLS</a></li>
+                                            <li><a href="#" onClick ="$(\'#export\').tableExport({type:\'doc\',escape:\'false\'});">Export DOC</a></li>
+                                            <li><a href="#" onClick ="$(\'#export\').tableExport({type:\'txt\',escape:\'false\'});">Export TXT</a></li>
+                                            <li><a href="#" onClick ="$(\'#export\').tableExport({type:\'csv\',escape:\'false\'});">Export CSV</a></li>
+                                            <li><a href="#" onClick ="$(\'#export\').tableExport({type:\'pdf\',pdfFontSize:\'7\',escape:\'false\'});">Export PDF</a></li>
+                                            <li><a href="#" onClick ="$(\'#export\').tableExport({type:\'sql\'});">Export SQL</a></li>
+                                            <li><a href="#" onClick ="$(\'#export\').tableExport({type:\'xml\',escape:\'false\'});">Export XML</a></li>
+                                            <li><a href="#" onClick ="$(\'#export\').tableExport({type:\'json\',escape:\'false\'});">Export JSON</a></li>
+                                        </ul>
+                                    </div>
+                            </div>
+                            <div class="content table-responsive table-full-width">
+                                <table id="export" class="table table-striped">
+                                    <thead>
+                                        <th>No</th>
+                                    	<th>Review ID</th>
+                                    	<th>Book ID</th>
+                                    	<th>Detail</th>
+                                    	<th>Status</th>
+                                        <th>Created</th>
+                                        <th>Username</th>
+                                    	<th>Updated at</th>
+                                        <th>Updated by</th>
+                                        <th>Manage</th>
+                                    </thead>
+                                    <tbody>';
+                $n=$data->metadata->{'number_item_first'};
+                foreach ($data->results as $name => $value) 
+	            {
+                    echo '<tr>';
+                    echo '<td>' . $n++ .'</td>';
+                    echo '<td>' . $value->{'ReviewID'} .'</td>';
+			        echo '<td>' . $value->{'BookID'} .'</td>';
+        			echo '<td>' . $value->{'Detail'} .'</td>';
+                	echo '<td>' . $value->{'Status'} .'</td>';
+                	echo '<td>' . $value->{'Created_at'} .'</td>';
+            	    echo '<td>' . $value->{'Username'} .'</td>';
+    	    		echo '<td>' . $value->{'Updated_at'} .'</td>';
+                    echo '<td>' . $value->{'Updated_by'} .'</td>';
+                    echo '<td><a href="#" data-toggle="modal" data-target="#'.$value->{'ReviewID'}.'"><i class="ti-pencil"></i> Edit</a></td>';
+	    	    	echo '</tr>';              
+                }
+                echo '</tbody>
+                </table>';
+
+                echo '</div>
+                </div>';
+
+                $pagination = new Pagination;
+                echo $pagination->makePagination($data,$_SERVER['PHP_SELF'].'?m=5&search='.$search);
+                
+                echo '</div>';
+
+                foreach ($data->results as $name=>$value){
+                    echo '<!-- Start Modal -->
+                        <div class="modal fade" id="'.$value->{'ReviewID'}.'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                          <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                <h4 class="modal-title" id="myModalLabel">Update Data Review</h4>
+                              </div>
+                              <form method="post" action="'.$_SERVER['PHP_SELF'].'?m=24&page='.$page.'&itemsperpage='.$itemsperpage.'&search='.$search.'">
+                              <div class="modal-body">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label>Detail</label>
+                                            <textarea name="detail" rows="3" type="text" placeholder="Input the detail review here..." class="form-control border-input">'.$value->{'Detail'}.'</textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label>Status</label>
+                                            <select name="status" type="text" style=\'max-height:200px; overflow-y:scroll; overflow-x:hidden;\' class="form-control border-input">';
+                                                if (!empty($datastatus)) {
+                                                            foreach ($datastatus->result as $name => $valuestatus) {
+                                                                echo '<option value="'.$valuestatus->{'StatusID'}.'" '.(($valuestatus->{'StatusID'} == $value->{'StatusID'})?'selected':'').'>'.$valuestatus->{'Status'}.'</option>';
+                                                            }
+                                                        }
+                                                    echo '</select>
+                                        </div>
+                                    </div>
+                                </div>
+                              </div>
+                              <div class="modal-footer">
+                                <button type="submit" name="submitdeletereview'.$value->{'ReviewID'}.'" class="btn btn-danger pull-left">Delete</button>
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                                <button type="submit" name="submitupdatereview'.$value->{'ReviewID'}.'" class="btn btn-primary">Update</button>
+                              </div>
+                              </form>
+                            </div>
+                          </div>
+                        </div>
+                    <!-- End Modal -->';
+                }
+            }
+            else
+            {
+                echo '<div class="col-md-12">
+                        <div class="card card-plain">
+                            <div class="header">
+                                <h4 class="title">Message: '.$data->{'message'}.'</h4>
+                            </div>
+                        </div>
+                    </div>';
+            } 
+        }
+?>
+                            </div>
+                        </div>
+                    </div>
+
+                
