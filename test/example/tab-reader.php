662cfa202e5ac65f0fc963d9477f2972bd09d5ae
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2017-06-13 17:00:11 +0700

    Change embed to iframe

--- test/example/tab-reader.php
+++ test/example/tab-reader.php
@@ -3,7 +3,9 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="flexible-container">
-							<iframe src="<?php echo (empty($_GET['file'])?'':Core::getInstance()->basepath.'/plugins/pdf/viewer.php?file='.$_GET['file'])?>" type="application/pdf" width="100%" height="100%" allowFullScreen></iframe>
+							<iframe src="<?php echo (empty($_GET['file'])?'':Core::getInstance()->basepath.'/plugins/pdf/viewer.php?file='.$_GET['file'])?>" type="application/pdf" width="100%" height="100%" allowFullScreen>
+                                <p>It appears your web browser doesn't support iframes.</p>
+                            </iframe>
                         </div>
                         <br>
                         <p class="text-center"><a href="<?php echo (empty($_GET['file'])?'':$_GET['file'])?>/download" download class="btn btn-success"><?php echo Core::lang('download')?></a></p>
