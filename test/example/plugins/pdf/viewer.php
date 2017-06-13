826ddb4c2c97772bae5588ea5f92d9c77163f368
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2017-06-13 17:56:17 +0700

    Disable Open File

--- test/example/plugins/pdf/viewer.php
+++ test/example/plugins/pdf/viewer.php
@@ -101,7 +101,7 @@ var DEFAULT_URL = '<?php echo (empty($_GET['file'])?'':$_GET['file'])?>';
               <span data-l10n-id="presentation_mode_label">Presentation Mode</span>
             </button>
 
-            <button id="secondaryOpenFile" class="secondaryToolbarButton openFile visibleLargeView" title="Open File" tabindex="52" data-l10n-id="open_file">
+            <button id="secondaryOpenFile" class="secondaryToolbarButton openFile visibleLargeView" title="Open File" tabindex="52" data-l10n-id="open_file" style="display: none;">
               <span data-l10n-id="open_file_label">Open</span>
             </button>
 
@@ -177,7 +177,7 @@ var DEFAULT_URL = '<?php echo (empty($_GET['file'])?'':$_GET['file'])?>';
                   <span data-l10n-id="presentation_mode_label">Presentation Mode</span>
                 </button>
 
-                <button id="openFile" class="toolbarButton openFile hiddenLargeView" title="Open File" tabindex="32" data-l10n-id="open_file">
+                <button id="openFile" class="toolbarButton openFile hiddenLargeView" title="Open File" tabindex="32" data-l10n-id="open_file" style="display: none;">
                   <span data-l10n-id="open_file_label">Open</span>
                 </button>
 
